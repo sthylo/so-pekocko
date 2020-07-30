ee2be546127725ab41de104f939a845e6ba4bddf
sthylo <63072156+sthylo@users.noreply.github.com>
2020-07-30 17:18:58 +0200

    Ajout de XSS-Clean et de dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
+require('dotenv').config({path: './.env'});
+
 const express = require("express"); // Framework d'infrastructure d'applications Web Node.js
 const bodyParser = require("body-parser"); // Gérer la demande POST provenant de l'application front-end
 const mongoose = require("mongoose"); // Package qui facilite les interactions avec notre base de données MongoDB
 const path = require("path"); // Gestionnaire de routage pour les images en upload et download
 const helmet = require("helmet"); // Configure de manière appropriée et sécurisée les en-têtes HTTP 
+const dotenv = require("dotenv"); // permet de créer une variable pour cacher les données de connexion à la base de données
+const xss = require('xss-clean'); // Permet de lutter contre les failles XSS (Cross-Site Scripting)
 
 const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user"); 
@@ -10,7 +14,7 @@ const userRoutes = require("./routes/user");
 // Connexion à la base de données
 mongoose  
   .connect(
-    "mongodb+srv://sthylo:jtesuce@so-pekocko.owh9w.mongodb.net/test?retryWrites=true&w=majority",
+    process.env.DB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true }
   ) 
   .then(() => console.log("Connexion à MongoDB réussie !"))
@@ -32,6 +36,18 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use(xss());
+app.use (helmet()); 
+app.use(bodyParser.json()); 
+
+app.use("/images", express.static(path.join(__dirname, "images"))); 
+
+app.use("/api/auth", userRoutes); 
+app.use("/api/sauces", sauceRoutes); 
+
+module.exports = app;
+
 
 app.use (helmet()); 
 app.use(bodyParser.json()); 
